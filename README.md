ceaef8046bc446ef9ca27d718499e9ac88cb36c0
arnold zephir <arnold.zephir@prevision.io>
2020-12-02 22:08:59 +0100

    [ feat  ] : generate tunes Frequencies

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mouzic
 Trying to make a piano Tune
+
+
+##  Egale temperament 
+
+on multiplie par 1.059
+
+261.626	277.183	293.665	311.127	329.628	349.228	369.994	391.995	415.305	440.000
--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+var contexteAudio = new (window.AudioContext || window.webkitAudioContext)();
+
+
 class Tune {
     constructor(targetId, params) {
         const { freq, name } = params
         const targetEl = document.getElementById(targetId)
   
-        // Creating the button
+        this.freq=freq
+
+        // Creating the button 
         this.tuneContainer = document.createElement("div")
         const playButton = document.createElement("button")
         playButton.addEventListener("click", this.play.bind(this))
@@ -14,7 +19,16 @@ class Tune {
     }
 
     play() {
-        console.log('play')
+        console.log(`Playing Frequency ${this.freq}`)
+
+        // create Oscillator node
+        var oscillator = contexteAudio.createOscillator();
+        oscillator.connect(contexteAudio.destination);
+        oscillator.type = 'sine';
+        oscillator.frequency.value = this.freq; // valeur en hertz
+
+        oscillator.start();
+        oscillator.stop(contexteAudio.currentTime + 1)
     }
 }
 
@@ -24,6 +38,10 @@ function launch() {
 
 
 launch()
-
+// C = Do
+const tunesName=['C','C#','D','D#','E','F','F#','G','G#', 'A','A#','B',]
+console.log(tunesName.length)
+const tuneValues = tunesName.map( (note, idx) => ({name:note, freq:261.626 * (1.05946**idx)}))
+console.log(tuneValues)
 const A = new Tune("keyboard", { freq:440, name:"A" })
 const B = new Tune("keyboard", { freq:457, name:"B" })
